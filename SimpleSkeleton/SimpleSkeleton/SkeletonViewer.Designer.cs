fb9ce6d079b66f5a3747c3e76283715ab0d2e7dd
Sophie Che <che.sophie@gmail.com>
2012-01-12 00:29:41 -0800

    first commit

---
+++ SimpleSkeleton/SimpleSkeleton/SkeletonViewer.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SimpleSkeleton
+{
+    partial class SkeletonViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.videoViewer = new System.Windows.Forms.PictureBox();
+            this.textJoints = new System.Windows.Forms.TextBox();
+            this.textStatus = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.videoViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // videoViewer
+            // 
+            this.videoViewer.Location = new System.Drawing.Point(4, 8);
+            this.videoViewer.Name = "videoViewer";
+            this.videoViewer.Size = new System.Drawing.Size(640, 480);
+            this.videoViewer.TabIndex = 0;
+            this.videoViewer.TabStop = false;
+            // 
+            // textJoints
+            // 
+            this.textJoints.Location = new System.Drawing.Point(650, 8);
+            this.textJoints.Multiline = true;
+            this.textJoints.Name = "textJoints";
+            this.textJoints.ReadOnly = true;
+            this.textJoints.Size = new System.Drawing.Size(408, 542);
+            this.textJoints.TabIndex = 2;
+            // 
+            // textStatus
+            // 
+            this.textStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textStatus.Location = new System.Drawing.Point(109, 513);
+            this.textStatus.Name = "textStatus";
+            this.textStatus.ReadOnly = true;
+            this.textStatus.Size = new System.Drawing.Size(191, 24);
+            this.textStatus.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(8, 513);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kinect Status";
+            // 
+            // SkeletonViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 562);
+            this.Controls.Add(this.textStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textJoints);
+            this.Controls.Add(this.videoViewer);
+            this.Name = "SkeletonViewer";
+            this.Text = "SkeletonViewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SkeletonViewer_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.videoViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox videoViewer;
+        private System.Windows.Forms.TextBox textJoints;
+        private System.Windows.Forms.TextBox textStatus;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ SimpleSkeleton/SimpleSkeleton/SkeletonViewer.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Microsoft.Research.Kinect.Nui;
+
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+
+namespace SimpleSkeleton
+{
+    public partial class SkeletonViewer : Form
+    {
+        #region Private States
+        PlanarImage videoimage;
+        //Kinect Runtime
+        private Runtime nui;
+        //Video params
+        private const ImageResolution IMAGE_RESOLUTION = ImageResolution.Resolution640x480;
+        private const int IMAGE_WIDTH = 640;
+        private const int IMAGE_HEIGHT = 480;
+        #endregion Private States
+
+        #region Form
+        public SkeletonViewer()
+        {
+            InitializeComponent();
+            InitKinect();
+        }
+
+        // The FormClosing event handler
+        private void SkeletonViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            nui.Uninitialize();
+        }
+        #endregion Form
+
+        #region Kinect Init
+        private void InitKinect()
+        {
+            // Detect Kinect
+            if (Runtime.Kinects.Count <= 0)
+            {
+                textStatus.Text = "No Kinect!";
+                return;
+            }
+
+            //use first Kinect
+            nui = Runtime.Kinects[0];
+            textStatus.Text = nui.Status.ToString();
+            if (nui.Status != KinectStatus.Connected)  return;
+
+            // Initialize to do skeletal tracking
+            nui.Initialize(RuntimeOptions.UseSkeletalTracking
+                    | RuntimeOptions.UseDepth | RuntimeOptions.UseDepthAndPlayerIndex
+                    | RuntimeOptions.UseColor);
+
+            // Open the video stream
+            //  return video images from the video camera and skeleton data.
+            nui.VideoStream.Open(ImageStreamType.Video, 2, IMAGE_RESOLUTION, ImageType.Color);
+
+            // Add event to receive skeleton data
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(SkeletonFrameReady);
+            nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(ImageFrameReady);
+
+            //to experiment, toggle TransformSmooth between true & false
+            // parameters used to smooth the skeleton data
+            nui.SkeletonEngine.TransformSmooth = true;
+            TransformSmoothParameters parameters = new TransformSmoothParameters();
+            parameters.Smoothing = 0.7f;
+            parameters.Correction = 0.3f;
+            parameters.Prediction = 0.4f;
+            parameters.JitterRadius = 1.0f;
+            parameters.MaxDeviationRadius = 0.5f;
+            nui.SkeletonEngine.SmoothParameters = parameters;
+        }
+        #endregion Kinect Init
+
+        #region Event Handlers
+        // The FrameReady event handler for displaying the image.
+        void ImageFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            videoimage = e.ImageFrame.Image;
+        }
+
+        // The FrameReady event handler for showing the skeleton
+        void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            if (videoimage.Bits == null) return;
+            
+            SkeletonFrame allSkeletons = e.SkeletonFrame;
+            textJoints.Text = "";
+            // There could be at most two fully tracked players.
+            foreach (SkeletonData data in allSkeletons.Skeletons) {
+                // Each SkeletonData contains a collection of Joints but may not actively tracked.
+                if (data.TrackingState == SkeletonTrackingState.NotTracked) continue;
+
+                foreach (Joint joint in data.Joints)
+                {
+                    if (joint.TrackingState == JointTrackingState.NotTracked) continue;
+
+                    textJoints.Text += (int)joint.ID + " " + joint.ID.ToString()
+                                        + " (" + joint.TrackingState.ToString() + ")\r\n";
+                    // Show joints' positions
+                    Vector posJt = joint.Position;
+                    textJoints.Text += "    Pos: X: " + joint.Position.X + ", Y: " + joint.Position.Y
+                                        + ", Z: " + joint.Position.Z;
+                    // Draw Joints
+                    //  from 3D to 2D
+                    float dX, dY;
+                    nui.SkeletonEngine.SkeletonToDepthImage(joint.Position, out dX, out dY);
+                    dX = Math.Max(0, Math.Min(dX * IMAGE_WIDTH, IMAGE_WIDTH));
+                    dY = Math.Max(0, Math.Min(dY * IMAGE_HEIGHT, IMAGE_HEIGHT));
+                    textJoints.Text += " => 2D X: " + (int)dX + ", Y: " + (int)dY + "\r\n";
+                    DrawJoint((int)dX, (int)dY);
+                    // Get pixel coodinate of the joint in the video bitmap
+                    //int x, y;
+                    //ImageViewArea iv = new ImageViewArea();
+                    //nui.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(
+                    //    IMAGE_RESOLUTION,
+                    //    iv,
+                    //    (int)dX, (int)dY,
+                    //    (short)0,
+                    //    out x, out y);
+                    //textJoints.Text += "\r\nX: " + x + ", Y: " + y;
+                    //DrawJoint(x, y);
+                }
+            }
+
+            Bitmap bmap = PImageToBitmap(videoimage);
+            videoViewer.Image = bmap;
+        }
+        #endregion
+
+        #region Private Methods
+        // Convert a PlanarImage to a bitmap
+        private Bitmap PImageToBitmap(PlanarImage PImage)
+        {
+            Bitmap bmap = new Bitmap(
+                PImage.Width,
+                PImage.Height,
+                PixelFormat.Format32bppRgb);
+            BitmapData bmapdata = bmap.LockBits(
+                new Rectangle(0, 0, PImage.Width,
+                                PImage.Height),
+                ImageLockMode.WriteOnly,
+                bmap.PixelFormat);
+            IntPtr ptr = bmapdata.Scan0;
+            Marshal.Copy(PImage.Bits,
+                0,
+                ptr,
+                PImage.Width *
+                PImage.BytesPerPixel *
+                PImage.Height);
+            bmap.UnlockBits(bmapdata);
+            return bmap;
+        }
+
+        private void DrawJoint(int x,int y)
+        {
+            if (x < 0 || y < 0 || x >= videoimage.Width || y >= videoimage.Height) return;
+            // Calculate index of pixel
+            int pt = (x + y * videoimage.Width) * videoimage.BytesPerPixel;
+            // Draw
+            for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+            int stride = videoimage.Width * videoimage.BytesPerPixel;
+            if (x >= 1)
+            {
+                pt -= 4;
+                for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                if (y >= 1)
+                {
+                    pt -= stride;
+                    for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                    pt += stride;
+                }
+                if (y < videoimage.Height-1)
+                {
+                    pt += stride;
+                    for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                    pt -= stride;
+                }
+                pt += 4;
+            }
+            if (x < videoimage.Width-1)
+            {
+                pt += 4;
+                for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                if (y >= 1)
+                {
+                    pt -= stride;
+                    for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                    pt += stride;
+                }
+                if (y < videoimage.Height - 1)
+                {
+                    pt += stride;
+                    for (int i = 0; i < 3; ++i) videoimage.Bits[pt + i] = 0xFF;
+                    pt -= stride;
+                }
+            }
+        }
+        #endregion
+    }
+}
